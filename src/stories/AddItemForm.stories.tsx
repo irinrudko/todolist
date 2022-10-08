4b2790e5fe62294dc3db8abfff720847db2777b1
Iryna Rudzko <irinrudko@gmail.com>
2022-10-08 18:43:43 +0400

    dev: add AddItemForm story

---
+++ src/stories/AddItemForm.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { AddItemForm } from '../AddItemForm';
+
+export default {
+  title: 'Todolist/AddItemForm',
+  component: AddItemForm,
+  argTypes: {
+    addItem: {action: 'title'}
+  }
+} as ComponentMeta<typeof AddItemForm>;
+
+
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+
+export const AddItemFormBasic = Template.bind({});
+AddItemFormBasic.args = {
+    label: 'write sth here'
+}
+
+
