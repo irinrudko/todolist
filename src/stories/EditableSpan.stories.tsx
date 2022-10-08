2717aa26cd59fbbf893ae4be121f9108063abb5b
Iryna Rudzko <irinrudko@gmail.com>
2022-10-08 17:51:30 +0400

    dev: add EditableSpan story

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { EditableSpan } from '../EditableSpan';
+import {action} from '@storybook/addon-actions';
+
+export default {
+  title: 'Todolist/EditableSpan',
+  component: EditableSpan,
+
+  argTypes: {
+    title: {control: 'text'}
+  },
+} as ComponentMeta<typeof EditableSpan>;
+
+
+const onChangeCallback = action('was changed to the value')
+
+const Template: ComponentStory<typeof EditableSpan> = (args) => <EditableSpan {...args} />;
+
+export const EditableSpanBasic = Template.bind({});
+EditableSpanBasic.args = {
+    onChange: onChangeCallback,
+    title: 'Dare to change me'
+}
+
+
