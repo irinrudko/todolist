de24e6f084a1db115000549fc9734e55a2e29f44
Iryna Rudzko <irinrudko@gmail.com>
2022-10-08 23:12:01 +0400

    test: add appWithRedux integration

---
+++ integration/appWithRedux.test.js
@@ -0,0 +1,8 @@
+describe('appWithRedux', () => {
+    it('appWithRedux, visually looks correct', async () => {
+        await page.goto('http://localhost:9009/iframe.html?args=&id=appwithredux--app-with-redux-basic&viewMode=story');
+        const image = await page.screenshot();
+         expect(image).toMatchImageSnapshot();
+    });
+ });
+ 
