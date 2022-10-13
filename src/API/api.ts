28913f481751da8d03c05426792d05d1179daba2
Iryna Rudzko <irinrudko@gmail.com>
2022-10-13 21:46:45 +0400

    refactor: add types for server response

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,5 +1,29 @@
 import axios from "axios"
 
+export type UpdateTaskModel = {
+    title: string
+    status: TaskStatuses
+    description: string
+    priority: TaskPriorities
+    startDate: string
+    deadline: string
+    completed: boolean
+}
+
+export enum TaskStatuses {
+    New = 0,
+    InProgress = 1,
+    Completed = 2,
+    Draft = 3
+}
+
+export enum TaskPriorities {
+    Low = 0,
+    Middle = 1,
+    Hi = 2,
+    Urgently = 3,
+    Later = 4
+}
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
@@ -28,8 +52,8 @@ export const tasksAPI = {
     createTask(todolistId: string, title: string) {
         return instance.post(`todo-lists/${todolistId}/tasks`, {title}).then(response => response.data)
     },
-    updateTask(todolistId: string, id: string, title: string, isDone: boolean) {
-        return instance.put(`todo-lists/${todolistId}/tasks/${id}`, {title, isDone}).then(response => response.data)
+    updateTask(todolistId: string, id: string, model: UpdateTaskModel) {
+        return instance.put(`todo-lists/${todolistId}/tasks/${id}`, model)
     },
     deleteTask(todolistId: string, id: string) {
         return instance.delete(`todo-lists/${todolistId}/tasks/${id}`).then(response => response.data)
