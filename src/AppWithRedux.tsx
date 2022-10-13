e7c7a7096b1c1202df98ef5a0bd2e7055c8eaf54
Iryna Rudzko <irinrudko@gmail.com>
2022-10-13 13:21:24 +0400

    refactor: update types

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -2,12 +2,13 @@ import { Container, Grid, Paper } from '@mui/material';
 import React, { useEffect } from 'react';
 import { AddNewTodolist } from './AddNewTodolist';
 import { Header } from './Header';
-import { TaskType, Todolist } from './Todolist';
+import { Todolist } from './Todolist';
 import { addTodolistAC } from './state/reducers/todolists-tasks-reducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppStateType } from './state/store';
 import { useCallback } from 'react';
 import {fetchTodoliststTC } from './state/reducers/todolist-reducer';
+import { TaskType } from './state/reducers/tasks-reducer';
 
 
 export type FilterValuesType = "all" | "active" | "completed";
