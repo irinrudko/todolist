6f67bd409306355c133d218903cde0de2ca08a33
Iryna Rudzko <irinrudko@gmail.com>
2022-10-26 23:34:35 +0400

    feat: add appReducer to Store

--- src/state/store.ts
+++ src/state/store.ts
@@ -3,11 +3,13 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { tasksReducer } from './reducers/tasks-reducer'
 import { todolistsReducer } from './reducers/todolist-reducer'
+import { appReducer } from '../app/app-reducer'
 
 export const rootReducer = combineReducers({
 	todolists: todolistsReducer,
 	tasks: tasksReducer,
 	auth: authReducer,
+	app: appReducer,
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
