1c4a1883384f57eb1cb114836aaf306504984a54
Iryna Rudzko <irinrudko@gmail.com>
2022-10-26 15:44:17 +0400

    feat: add loginTC

--- src/state/reducers/auth-reducer.ts
+++ src/state/reducers/auth-reducer.ts
@@ -1,6 +1,13 @@
+import { Dispatch } from 'redux'
+import { LoginParamsData, userAPI } from '../../API/user-api'
+
 const initialState: AuthInitialStateType = {
 	isLoggedIn: false,
 }
+
+//TODO
+//create useAppDispatch
+//create useAppSelector
 
 export const authReducer = (state: AuthInitialStateType = initialState, action: AuthActionsType): AuthInitialStateType => {
 	switch (action.type) {
@@ -15,6 +22,15 @@ export const authReducer = (state: AuthInitialStateType = initialState, action: 
 export const setIsLoggedInAC = (isLoggedIn: boolean) => ({ type: 'AUTH/SET-IS-LOGGED-IN', isLoggedIn } as const)
 
 //thunks
+export const loginTC = (data: LoginParamsData) => (dispatch: Dispatch) => {
+	userAPI
+		.login(data)
+		.then((res) => {
+			dispatch(setIsLoggedInAC(true))
+			// dispatch(setUserDataAC(res))
+		})
+		.catch((err: any) => {})
+}
 
 //types
 type AuthInitialStateType = {
