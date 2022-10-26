1990f7b76cc06c97d43d4de0f0411f6ae6be9f89
Iryna Rudzko <irinrudko@gmail.com>
2022-10-27 00:00:52 +0400

    refactor: update API-KEY

--- src/API/instance.ts
+++ src/API/instance.ts
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const instance = axios.create({
 	baseURL: 'https://social-network.samuraijs.com/api/1.1/',
 	withCredentials: true,
-	headers: { 'API-KEY': 'dcfcafd7-85ce-4812-a7da-28eb32543b9d' },
+	headers: { 'API-KEY': '77e9ebd2-efbf-4b5a-82fe-4b2ad908357b' },
 })
