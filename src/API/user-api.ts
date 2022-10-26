361250c3bd12c48f42d7c4f862bf3daa381e21d2
Iryna Rudzko <irinrudko@gmail.com>
2022-10-26 21:47:58 +0400

    fix: add .then to userAPI login

--- src/API/user-api.ts
+++ src/API/user-api.ts
@@ -5,7 +5,7 @@ export const userAPI = {
 		return instance.get('auth/me')
 	},
 	login(data: LoginParamsData) {
-		return instance.post('auth/login', data)
+		return instance.post('auth/login', data).then((res) => res.data)
 	},
 	logout() {
 		return instance.delete('auth/login')
