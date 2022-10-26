62ee7686b49cfa1a10761e9ecbfcbd9d10c5798b
Iryna Rudzko <irinrudko@gmail.com>
2022-10-26 15:14:44 +0400

    feat: add userAPI

--- src/API/user-api.ts
+++ src/API/user-api.ts
@@ -1,1 +1,21 @@
 import { instance } from './instance'
+
+export const userAPI = {
+	me() {
+		return instance.get('auth/me')
+	},
+	login(data: LoginParamsType) {
+		return instance.post('auth/login', data)
+	},
+	logout() {
+		return instance.delete('auth/login')
+	},
+}
+
+//types
+export type LoginParamsType = {
+	email: string
+	password: string
+	rememberMe: boolean
+	captcha?: string
+}
