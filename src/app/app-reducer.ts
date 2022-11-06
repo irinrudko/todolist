1b0a8eee53e56869129968de46223da5a4687e7c
Iryna Rudzko <irinrudko@gmail.com>
2022-11-06 17:46:34 +0400

    feat: add error to app-reducer

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -5,6 +5,7 @@ import { setIsLoggedInAC } from '../state/reducers/auth-reducer'
 const initialState = {
 	isInitialized: false,
 	status: 'loading' as RequestStatusType,
+	error: null,
 }
 
 export const appReducer = (state: AppInitialStateType = initialState, action: AppReducerActionsType): AppInitialStateType => {
@@ -13,6 +14,8 @@ export const appReducer = (state: AppInitialStateType = initialState, action: Ap
 			return { ...state, isInitialized: action.value }
 		case 'APP/SET-STATUS':
 			return { ...state, status: action.status }
+		case 'APP/SET-ERROR':
+			return { ...state, error: action.error }
 		default:
 			return { ...state }
 	}
@@ -21,6 +24,7 @@ export const appReducer = (state: AppInitialStateType = initialState, action: Ap
 //action creators
 export const setAppInitializedAC = (value: boolean) => ({ type: 'APP/SET-IS-INITIALIZED', value } as const)
 export const setAppStatusAC = (status: RequestStatusType) => ({ type: 'APP/SET-STATUS', status } as const)
+export const setAppErrorAC = (error: string | null) => ({ type: 'APP/SET-ERROR', error } as const)
 
 //thunks
 export const initializeAppTC = () => (dispatch: Dispatch) => {
@@ -38,7 +42,14 @@ export const initializeAppTC = () => (dispatch: Dispatch) => {
 		})
 }
 //types
-type AppInitialStateType = typeof initialState
+type AppInitialStateType = {
+	isInitialized: boolean
+	status: RequestStatusType
+	error: string | null
+}
 type RequestStatusType = 'idle' | 'loading' | 'success' | 'fail'
 
-export type AppReducerActionsType = ReturnType<typeof setAppInitializedAC> | ReturnType<typeof setAppStatusAC>
+export type AppReducerActionsType =
+	| ReturnType<typeof setAppInitializedAC>
+	| ReturnType<typeof setAppStatusAC>
+	| ReturnType<typeof setAppErrorAC>
