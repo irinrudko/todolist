9eb1edf068fb15895e5da1530f2991c810d79d87
Iryna Rudzko <irinrudko@gmail.com>
2022-11-06 18:15:58 +0400

    feat: validate input to take no more than 25 symbols

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -39,11 +39,15 @@ export const AddItemForm: React.FC<AddItemFormType> = React.memo((props) => {
 		}
 	}
 	const addItem = () => {
-		if (title.trim() !== '') {
+		if (title.trim() !== '' && title.length < 26) {
 			props.addItem(title.trim())
 			setTitle('')
 		} else {
 			setError('Title is required')
+		}
+
+		if (title.length >= 26) {
+			setError('Title cannot be more than 25 symbols')
 		}
 	}
 
