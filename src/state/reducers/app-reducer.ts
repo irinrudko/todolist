af6ce6fe7a2683d801dabc1724561656d939ebcf
Iryna Rudzko <irinrudko@gmail.com>
2022-11-15 11:37:00 +0400

    refactor: add slice in auth-reducer

--- src/state/reducers/app-reducer.ts
+++ src/state/reducers/app-reducer.ts
@@ -32,7 +32,7 @@ export const initializeAppTC = () => (dispatch: Dispatch) => {
 		.me()
 		.then((res) => {
 			if (res.data.resultCode === 0) {
-				dispatch(setIsLoggedInAC(true))
+				dispatch(setIsLoggedInAC({ isLoggedIn: true }))
 			} else {
 			}
 		})
--- src/state/reducers/auth-reducer.ts
+++ src/state/reducers/auth-reducer.ts
@@ -1,22 +1,37 @@
 import { AppThunk } from './../store'
 import { LoginParamsData, userAPI } from '../../API/user-api'
 import { setAppStatusAC } from './app-reducer'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 const initialState: AuthInitialStateType = {
 	isLoggedIn: false,
 }
 
-export const authReducer = (state: AuthInitialStateType = initialState, action: AuthActionsType): AuthInitialStateType => {
-	switch (action.type) {
-		case 'AUTH/SET-IS-LOGGED-IN':
-			return { ...state, isLoggedIn: action.isLoggedIn }
-		default:
-			return state
-	}
-}
+const slice = createSlice({
+	name: 'auth',
+	initialState: initialState,
+	reducers: {
+		setIsLoggedInAC(state, action: PayloadAction<{ isLoggedIn: boolean }>) {
+			state.isLoggedIn = action.payload.isLoggedIn
+		},
+	},
+})
+
+export const authReducer = slice.reducer
+
+export const setIsLoggedInAC = slice.actions.setIsLoggedInAC
+
+// export const authReducer = (state: AuthInitialStateType = initialState, action: AuthActionsType): AuthInitialStateType => {
+// 	switch (action.type) {
+// 		case 'AUTH/SET-IS-LOGGED-IN':
+// 			return { ...state, isLoggedIn: action.isLoggedIn }
+// 		default:
+// 			return state
+// 	}
+// }
 
 //action creators
-export const setIsLoggedInAC = (isLoggedIn: boolean) => ({ type: 'AUTH/SET-IS-LOGGED-IN', isLoggedIn } as const)
+// export const setIsLoggedInAC = (isLoggedIn: boolean) => ({ type: 'AUTH/SET-IS-LOGGED-IN', isLoggedIn } as const)
 
 //thunks
 export const loginTC =
@@ -26,7 +41,7 @@ export const loginTC =
 
 		userAPI.login(data).then((res) => {
 			if (res.resultCode === 0) {
-				dispatch(setIsLoggedInAC(true))
+				dispatch(setIsLoggedInAC({ isLoggedIn: true }))
 				dispatch(setAppStatusAC('success'))
 			}
 		})
@@ -37,7 +52,7 @@ export const logoutTC = (): AppThunk => (dispatch) => {
 
 	userAPI.logout().then((res) => {
 		if (res.resultCode === 0) {
-			dispatch(setIsLoggedInAC(false))
+			dispatch(setIsLoggedInAC({ isLoggedIn: false }))
 			dispatch(setAppStatusAC('success'))
 		}
 	})
