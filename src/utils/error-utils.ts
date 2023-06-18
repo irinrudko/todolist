8f641292a8178dcc2319315d4c69d64986edd75a
Iryna Rudzko <irinrudko@gmail.com>
2023-06-18 21:43:59 +0200

    refactor: error for RTK

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,9 +1,14 @@
-import { Dispatch } from 'redux'
+import { Dispatch } from '@reduxjs/toolkit'
 import { appActions } from '../state/reducers/app-reducer'
+import axios, { AxiosError } from 'axios'
 
-export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch) => {
-	dispatch(
-		appActions.setAppErrorAC({ error: error.message ? error.message : 'Sorry, some error occurred. Please try again later' })
-	)
+export const handleServerNetworkError = (e: unknown, dispatch: Dispatch) => {
+	const err = e as Error | AxiosError<{ error: string }>
+	if (axios.isAxiosError(err)) {
+		const error = err.message ? err.message : 'Some error occurred'
+		dispatch(appActions.setAppErrorAC({ error }))
+	} else {
+		dispatch(appActions.setAppErrorAC({ error: `Native error ${err.message}` }))
+	}
 	dispatch(appActions.setAppStatusAC({ status: 'fail' }))
 }
